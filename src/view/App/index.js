01976a26787665c06b794f3aeb0e30c0e657b35b
Silvano Gomes do Amaral <silvano.amaral@spcbr.int.br>
2020-03-24 08:39:29 -0300

    first commit

---
+++ src/view/App/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <div className="App">
+        App js
+      </div>
+    </div>
+  );
+}
+
+export default App;
